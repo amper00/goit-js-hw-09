cd9e270d87ad7fa75c65e6c13d6e566a2519d65b
Daniel Szul <amper00@gmail.com>
2024-02-28 12:46:11 +0100

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector(`[data-start]`);
+const stopBtn = document.querySelector(`[data-stop]`);
+const background = document.querySelector('body');
 
+let timerId = null;
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener(`click`, () => {
+  timerId = setInterval(() => {
+    background.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener(`click`, () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
